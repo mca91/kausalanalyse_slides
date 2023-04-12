bb0bf4cec69098ba0ed7681891517c257aac5fd5
mca91 <martin.emwikts@googlemail.com>
2023-04-12 17:47:12 +0200

    rebuild

--- printing.R
+++ printing.R
@@ -1,4 +1,6 @@
 
+library(tidyverse)
+
 # install.packages("renderthis")
 # Chrome browser needs to be installed for this to work!
 
@@ -13,15 +15,15 @@ Rmd_files <- dir(recursive = T) %>%
 file_nms <- Rmd_files %>% str_remove(pattern = "\\.rmd|.Rmd$")
 
 # render files to HTML moon_reader format
-walk(
-  .x = Rmd_files, 
-  .f = ~ rmarkdown::render(paste0("~/git_projects/KA_slides/", .x), 'xaringan::moon_reader')
-)
+# walk(
+#   .x = Rmd_files, 
+#   .f = ~ rmarkdown::render(paste0("~/git_projects/KA_slides/", .x), 'xaringan::moon_reader')
+# )
 
-# generate PDFs (takes a moment)
+# generate HTML and then PDF files (takes a moment)
 walk(
   .x = paste0(base, "/", Rmd_files),
-  .f = ~ renderthis::to_pdf(., )
+  .f = ~ renderthis::to_pdf(.)
 )
 
 
