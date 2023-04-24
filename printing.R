9be23299981f1feb2e54a7826aa3a1f909df9d7d
mca91 <martin.emwikts@googlemail.com>
2023-04-24 17:58:40 +0200

    updated introduction slide

--- printing.R
+++ printing.R
@@ -14,12 +14,14 @@ Rmd_files <- dir(recursive = T) %>%
 
 file_nms <- Rmd_files %>% str_remove(pattern = "\\.rmd|.Rmd$")
 
+# old:
 # render files to HTML moon_reader format
 # walk(
 #   .x = Rmd_files, 
 #   .f = ~ rmarkdown::render(paste0("~/git_projects/KA_slides/", .x), 'xaringan::moon_reader')
 # )
 
+# new: 
 # generate HTML and then PDF files (takes a moment)
 walk(
   .x = paste0(base, "/", Rmd_files),
