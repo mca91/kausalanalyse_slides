621967a7751d3f3c3c831567aeba26521b70c721
mca91 <martin.emwikts@googlemail.com>
2023-04-27 09:43:35 +0200

    fixed formatting issue due to home button

--- printing.R
+++ printing.R
@@ -14,15 +14,7 @@ Rmd_files <- dir(recursive = T) %>%
 
 file_nms <- Rmd_files %>% str_remove(pattern = "\\.rmd|.Rmd$")
 
-# old:
-# render files to HTML moon_reader format
-# walk(
-#   .x = Rmd_files, 
-#   .f = ~ rmarkdown::render(paste0("~/git_projects/KA_slides/", .x), 'xaringan::moon_reader')
-# )
-
-# new: 
-# generate HTML and then PDF files (takes a moment)
+# generate HTML and PDF files (for all chapters; takes a moment)
 walk(
   .x = paste0(base, "/", Rmd_files),
   .f = ~ renderthis::to_html(.)
@@ -33,6 +25,14 @@ walk(
   .f = ~ renderthis::to_pdf(.)
 )
 
-# renderthis::to_pdf("~/git_projects/KA_slides/Introduction/Introduction.Rmd")
+renderthis::to_pdf("~/git_projects/KA_slides/Introduction/Introduction.Rmd")
+renderthis::to_html("~/git_projects/KA_slides/Introduction/Introduction.Rmd")
 
+renderthis::to_pdf("~/git_projects/KA_slides/ResearchQuestions/ResearchQuestions.Rmd")
+renderthis::to_html("~/git_projects/KA_slides/ResearchQuestions/ResearchQuestions.Rmd")
 
+renderthis::to_pdf("~/git_projects/KA_slides/TreatmentEffects/TreatmentEffects.Rmd")
+renderthis::to_html("~/git_projects/KA_slides/TreatmentEffects/TreatmentEffects.Rmd")
+
+renderthis::to_pdf("~/git_projects/KA_slides/Matching/Matching.Rmd")
+renderthis::to_html("~/git_projects/KA_slides/Matching/Matching.Rmd")
